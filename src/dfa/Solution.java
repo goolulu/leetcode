0bb1a647e154f52ceded2bdf6a4964e5245c6658
Abel <hrnweiruan@outlook.com>
2021-04-11 16:09:41 +0800

    杨辉三角

--- src/dfa/Solution.java
+++ src/dfa/Solution.java
@@ -120,7 +120,7 @@ public class Solution {
     * @Author: huang 
     * @Date: 2021/4/11-11:11
     */
-    public int[] plusOne(int[] digits) {
+    /*public int[] plusOne(int[] digits) {
         int[] result = new int[digits.length + 1];
         System.arraycopy(digits,0,result,1,digits.length);
         int jinwei = 1;
@@ -138,9 +138,76 @@ public class Solution {
             return digits;
         }
         return result;
+    }*/
+    //方法二
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length-1; i >=0 ; i--) {
+            digits[i]++;
+            digits[i] = digits[i]%10;
+            if (digits[i] != 0) {
+                return digits;
+            }
+        }
+        if (digits[0] == 0) {
+            digits = new int[digits.length + 1];
+            digits[0] = 1;
+
+        }
+        return digits;
+
+    }
+    /** 
+    * @Description: 88. 合并两个有序数组
+    * @Param:  
+    * @return:  
+    * @Author: huang 
+    * @Date: 2021/4/11-12:00
+    */
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int n1 = 0;
+        int n2 = 0;
+        int  cur = 0;
+        int[] result = new int[nums1.length];
+        while (n1 < m || n2 < n) {
+            if (n1 == m) {
+                cur = nums2[n2++];
+            } else if (n2 == n) {
+                cur = nums1[n1++];
+            } else if (nums1[n1] < nums2[n2]) {
+                cur = nums1[n1++];
+            } else {
+                cur = nums2[n2++];
+            }
+            result[n1+n2-1] = cur;
+        }
+        System.arraycopy(result,0,nums1,0,nums1.length);
+
+    }
+    /** 
+    * @Description: 118. 杨辉三角。
+    * @Param:  
+    * @return:  
+    * @Author: huang 
+    * @Date: 2021/4/11-16:02
+    */
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ret = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row  = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    //等于上一行的前一个加上一行对应index
+                    row.add(ret.get(i - 1).get(j - 1) + ret.get(i - 1).get(j));
+                }
+            }
+            ret.add(row);
+        }
+        return ret;
     }
     public static void main(String[] args) {
         Solution solution = new Solution();
-        solution.plusOne(new int[]{4,3,2,1});
+        solution.generate(5);
     }
 }
