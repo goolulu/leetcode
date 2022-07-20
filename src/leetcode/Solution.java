ba328fa0f26f00db42156977d9db6faf1c09bba8
Abel <hrnweiruan@outlook.com>
2022-07-20 23:23:44 +0800

    后序遍历

--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -1628,6 +1628,20 @@ public class Solution {
 
     }
 
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        postOrder(root, res);
+        return res;
+    }
+
+    private void postOrder(TreeNode node, List<Integer> res) {
+        if (node == null) {
+            return;
+        }
+        postOrder(node.left, res);
+        postOrder(node.right, res);
+        res.add(node.val);
+    }
     public static void main(String[] args) {
         Solution s = new Solution();
         s.sortedArrayToBST(new int[] {1, 3});
