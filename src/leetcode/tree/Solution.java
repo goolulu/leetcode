966b168cfb15f93b5da5b42e57cc1fa8e1c68a3d
Abel <hrnweiruan@outlook.com>
2022-10-08 19:17:02 +0800

    226. 翻转二叉树

--- src/leetcode/tree/Solution.java
+++ src/leetcode/tree/Solution.java
@@ -200,28 +200,47 @@ public class Solution {
         return hasPathSum_Recursive(root.left, targetSum - root.val) || hasPathSum_Recursive(root.right, targetSum - root.val);
     }
 
+    /**
+     * 226. 翻转二叉树
+     * @param root
+     * @return
+     */
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        root.left = right;
+        root.right = left;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
 
     public static void main(String[] args) {
-        TreeNode node1 = new TreeNode(5);
-        TreeNode node2 = new TreeNode(4);
-        TreeNode node3 = new TreeNode(8);
-        TreeNode node4 = new TreeNode(11);
-        TreeNode node5 = new TreeNode(13);
-        TreeNode node6 = new TreeNode(4);
-        TreeNode node7 = new TreeNode(7);
-        TreeNode node8 = new TreeNode(2);
-        TreeNode node9 = new TreeNode(1);
+        TreeNode node1 = new TreeNode(4);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(7);
+        TreeNode node4 = new TreeNode(1);
+        TreeNode node5 = new TreeNode(3);
+        TreeNode node6 = new TreeNode(6);
+        TreeNode node7 = new TreeNode(9);
+//        TreeNode node8 = new TreeNode(2);
+//        TreeNode node9 = new TreeNode(1);
 
 
         node1.left = node2;
         node1.right = node3;
 
         node2.left = node4;
-        node3.left = node5;
-        node3.right = node6;
+        node2.right = node5;
+        node3.left = node6;
+        node3.right = node7;
         node4.left = node7;
-        node4.right = node8;
-        node6.right = node9;
+//        node4.right = node8;
+//        node6.right = node9;
 //        node5.right = node7;
 
 //        node3.left = node4;
@@ -231,5 +250,6 @@ public class Solution {
 //        node4.right = node7;
 
         Solution solution = new Solution();
+        solution.invertTree(node1);
     }
 }
