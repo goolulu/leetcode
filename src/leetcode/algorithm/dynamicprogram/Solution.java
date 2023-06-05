71f12994002d7c736eecd65bff88e6557764a06a
huangrenning <1715818266@qq.com>
2023-06-05 13:58:34 +0800

    feat: 最长公共前缀

--- src/leetcode/algorithm/dynamicprogram/Solution.java
+++ src/leetcode/algorithm/dynamicprogram/Solution.java
@@ -1,10 +1,13 @@
 package leetcode.algorithm.dynamicprogram;
 
+import java.util.stream.Stream;
+
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(longestPalindrome("babc"));
-        System.out.println(longestPalindromeDp("babc"));
+        System.out.println(longestCommonPrefix(Stream.of("flower", "flow", "flight").toArray(String[]::new)));
+        System.out.println(longestCommonPrefix(Stream.of("dog", "racecar", "car").toArray(String[]::new)));
     }
+
 
     /**
      * 最长回文子串 暴力
@@ -85,5 +88,32 @@ public class Solution {
         }
         return s.substring(begin, begin + max);
     }
+
+    public static String longestCommonPrefix(String[] strs) {
+        int len = strs.length;
+        if (len == 1) {
+            return strs[0];
+        }
+
+        String commonPrefix = strs[0];
+        for (int i = 1; i < len; i++) {
+
+            String curr = strs[i];
+            int minLen = Math.min(commonPrefix.length(), curr.length());
+            String newC = "";
+            for (int j = 0; j < minLen; j++) {
+                char currChar = curr.charAt(j);
+                char commonChar = commonPrefix.charAt(j);
+                if (currChar == commonChar) {
+                    newC = newC + currChar;
+                } else {
+                    break;
+                }
+            }
+            commonPrefix = newC;
+        }
+        return commonPrefix;
+    }
+
 
 }
